6be6703c95d07fdd506d7559be8400dbb8834403
Alfred <54032594+spynetS@users.noreply.github.com>
2020-03-18 17:23:31 +0100

    Add files via upload

---
+++ mywebsite/javascript/main.js
@@ -0,0 +1,38 @@
+//Checks if mouse colides with sidebar
+const mouseTarget = document.getElementById('sidebar');
+mouseTarget.addEventListener('mouseenter', e => {
+  toggleSidebar();
+});
+
+function toggleSidebar()
+{
+	document.getElementById('sidebar').classList.toggle('active');
+}
+
+const submitbut = document.getElementById('submit');
+
+
+function submit()
+{
+	
+
+	var subjekt = document.getElementById('subjektName').value;
+	var task = document.getElementById('taskName').value;
+	var date = document.getElementById('dateName').value;
+	if(subjekt ===""||task===""||date==="")
+	{
+		alert("Write subjekt and task and select date");
+	}
+	else
+	{	
+	const ul = document.querySelector('.MainList');
+	var li = document.createElement("li");
+  	li.appendChild(document.createTextNode(subjekt+" "+task+" "+date));
+  	ul.appendChild(li);
+	document.getElementById('dateName').value= "";
+	document.getElementById('subjektName').value= "";
+	document.getElementById('taskName').value= "";
+	}
+
+}
+

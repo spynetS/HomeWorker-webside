bc8740e318f626dc90e96ba5e66ae643c94b5d2c
Alfred <54032594+spynetS@users.noreply.github.com>
2020-03-19 01:26:02 +0100

    login
    Adderade så om man skrev fel lösen  ord kan man se det

--- mywebsite/javascript/vocabulary.js
+++ mywebsite/javascript/vocabulary.js
@@ -1,23 +1,52 @@
 const startBut = document.getElementById('startBut');
 
+$(document).ready(function(){
+    $('#TextBoxId').keypress(function(e){
+      if(e.keyCode==13)
+      $('#linkadd').click();
+    });
+});
 
 function StartVocProd()
 {
-	const prod = document.getElementById('vocProd');
-	const firstWord = document.getElementById('firstWord');
+	var prod = document.getElementById('vocProd');
+	var firstWord = document.getElementById('firstWord');
 	firstWord.value = prod.value;
 }
 function Correct()
 {
-	const prod = document.getElementById('vocProd');
-	const firstWord = document.getElementById('firstWord');
-	const secundWord = document.getElementById('secundWord');
+	var prod = document.getElementById('vocProd');
+	var firstWord = document.getElementById('firstWord');
+	var secundWord = document.getElementById('secundWord');
+	var right = document.getElementById('rightAnswer');
+	var wordOn = document.getElementById('wordOn');
 	if(firstWord.value===secundWord.value)
 	{
-		alert("Correct");
+		right.innerText = "Right";
+		right.style.color = "#48F86E";
+		firstWord.value = firstWord.value+"asd";
+		right.style.fontSize = "22px";
+		var int = parseInt(wordOn.innerText);
+		int++;
+		wordOn.innerText = int;
+		setTimeout(removeLabel, 3000)
 	}
 	else
 	{
-		alert("Wrong");
+		right.innerText = "Wrong";
+		right.style.color = "red";
+		right.style.fontSize = "34px";
+		setTimeout(normalSize, 100)
 	}
+}
+function normalSize()
+{
+	var right = document.getElementById('rightAnswer');
+	right.style.fontSize = "22px";
+}
+
+function removeLabel()
+{
+	var right = document.getElementById('rightAnswer');
+	right.style.fontSize = "0px";
 }
